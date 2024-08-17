1b0d253a289aff43b03fde47126ab0593960e63f
trinhan2103la <trinhan212003la@gmail.com>
2024-08-17 10:28:08 +0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/ap", (req, res) => res.send("Hello My Best Friend."));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
