860fede25f4c7531b5943fd306a6962a9c01a817
trinhan2103la <trinhan212003la@gmail.com>
2024-08-19 14:35:19 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -5,15 +5,25 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+//Static file
+app.use(express.static(path.join(__dirname, "public")));
+
 // HTTP logger
 app.use(morgan("combined"));
 
 // Template engine
-app.engine("handlebars", handlebars.engine());
-app.set("view engine", "handlebars");
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.render("home"));
+
+app.get("/news", (req, res) => res.render("news"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
