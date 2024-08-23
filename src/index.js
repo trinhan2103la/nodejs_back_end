1a5761204b241eec4d48a0da54a22e296ab3db6e
trinhan2103la <trinhan212003la@gmail.com>
2024-08-23 11:46:39 +0700

    [update] route

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ const morgan = require("morgan");
 const handlebars = require("express-handlebars");
 const path = require("path");
 const app = express();
+
 const port = 3000;
+
+const route = require("./routes");
 
 //Static file
 app.use(express.static(path.join(__dirname, "public")));
+
+// Middleware or other app.use() calls
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
 
 // HTTP logger
 app.use(morgan("combined"));
@@ -21,11 +32,8 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => res.render("home"));
-
-app.get("/news", (req, res) => res.render("news"));
-
-app.get("/search", (req, res) => res.render("search"));
+// Route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
